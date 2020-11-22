3c571ffc4357864ffbc45734feeaba77fb41f3a2
DiemHieu <71001847+DiemHieu@users.noreply.github.com>
2020-11-23 00:08:09 +0700

    Create StartQuiz.java

---
+++ DiemDangHieu/ProjectDoulingo/app/src/main/java/com/example/doulingo/StartQuiz.java
@@ -0,0 +1,36 @@
+package com.example.doulingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.style.BackgroundColorSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class StartQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+        Button buttonquiz1 = findViewById(R.id.buttonquiz1);
+        buttonquiz1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
+
+
+
