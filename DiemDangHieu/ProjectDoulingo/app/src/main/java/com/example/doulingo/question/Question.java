c55552de7d03e5e59e339df266f381ae89a41100
DiemHieu <71001847+DiemHieu@users.noreply.github.com>
2020-12-04 01:13:47 +0700

    Create Question.java

---
+++ DiemDangHieu/ProjectDoulingo/app/src/main/java/com/example/doulingo/question/Question.java
@@ -0,0 +1,82 @@
+package com.example.doulingo.question;
+
+public class Question {
+
+    private int _id;
+    private String question;
+    private String ans_a;
+    private String ans_b;
+    private String ans_c;
+    private String result;
+    private int num_exam;
+
+    public Question(int _id, String question, String ans_a, String ans_b, String ans_c, String result,int num_exam) {
+        this._id = _id;
+        this.question = question;
+        this.ans_a = ans_a;
+        this.ans_b = ans_b;
+        this.ans_c = ans_c;
+        this.result = result;
+        this.num_exam = num_exam;
+    }
+
+    public Question() {
+
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAns_a() {
+        return ans_a;
+    }
+
+    public void setAns_a(String ans_a) {
+        this.ans_a = ans_a;
+    }
+
+    public String getAns_b() {
+        return ans_b;
+    }
+
+    public void setAns_b(String ans_b) {
+        this.ans_b = ans_b;
+    }
+
+    public String getAns_c() {
+        return ans_c;
+    }
+
+    public void setAns_c(String ans_c) {
+        this.ans_c = ans_c;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public int getNum_exam() {
+        return num_exam;
+    }
+
+    public void setNum_exam(int num_exam) {
+        this.num_exam = num_exam;
+    }
+}
