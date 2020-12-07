61e2bbdfe166cc3632a18b3d68606e64b3eca199
DiemHieu <71001847+DiemHieu@users.noreply.github.com>
2020-12-07 23:56:21 +0700

    Create MainActivity6Q1.java

---
+++ 10/ProjectDoulingo/app/src/main/java/com/example/doulingo/MainActivity6Q1.java
@@ -0,0 +1,16 @@
+package com.example.doulingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity6Q1 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6_q1);
+    }
+}
