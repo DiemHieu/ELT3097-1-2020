dec38e6dd8d2cb932b866dc6222530fc3dd9e19c
xuanduc1 <71000234+xuanduc1@users.noreply.github.com>
2020-12-08 20:13:19 +0700

    add

---
+++ 10/ProjectDoulingo/app/src/main/java/com/example/doulingo/MainActivity3Q1.java
@@ -0,0 +1,112 @@
+package com.example.doulingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity3Q1 extends AppCompatActivity {
+
+    Button button4;
+    Button button;
+    Button button1;
+    Button button2;
+    TextView textView3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3_q1);
+        button4 = findViewById(R.id.button4);
+        button = findViewById(R.id.button);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        textView3 = findViewById(R.id.textView3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setBackgroundResource(R.drawable.button_click_design);
+                button1.setBackgroundResource(R.drawable.button_answer_design);
+                button2.setBackgroundResource(R.drawable.button_answer_design);
+                button4.setBackgroundResource(R.drawable.button_question_click_design);
+                button4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String Button4Text = button4.getText().toString();
+                        textView3.setText("\t Sai rồi.Đáp án đúng là:\n  She is drinking water");
+                        textView3.setBackgroundResource(R.drawable.text_design_false);
+                        button4.setBackgroundResource(R.drawable.button_qs_click_false);
+                        button4.setText("Tiếp tục");
+                        if(Button4Text.equals("Tiếp tục")){
+                            openActivity4();
+                        }
+                    }
+                });
+
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button1.setBackgroundResource(R.drawable.button_click_design);
+                button.setBackgroundResource(R.drawable.button_answer_design);
+                button2.setBackgroundResource(R.drawable.button_answer_design);
+                button4.setBackgroundResource(R.drawable.button_question_click_design);
+                button4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String Button4Text = button4.getText().toString();
+                        textView3.setText("\t Sai rồi.Đáp án đúng là:\n  She is drinking water");
+                        textView3.setBackgroundResource(R.drawable.text_design_false);
+                        button4.setBackgroundResource(R.drawable.button_qs_click_false);
+                        button4.setText("Tiếp tục");
+                        if(Button4Text.equals("Tiếp tục")){
+                            openActivity4();
+                        }
+                    }
+                });
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button2.setBackgroundResource(R.drawable.button_click_design);
+                button.setBackgroundResource(R.drawable.button_answer_design);
+                button1.setBackgroundResource(R.drawable.button_answer_design);
+
+                button4.setBackgroundResource(R.drawable.button_question_click_design);
+                button4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String Button4Text = button4.getText().toString();
+                        textView3.setText("\t Chính xác!");
+                        textView3.setBackgroundResource(R.drawable.text_design_true);
+                        button4.setText("Tiếp tục");
+                        if(Button4Text.equals("Tiếp tục")){
+                            openActivity4();
+                        }
+                    }
+                });
+            }
+        });
+    }
+    public void clickexit(View v){
+        // moveTaskToBack(true);
+        //android.os.Process.killProcess(android.os.Process.myPid());
+        // System.exit(1);
+        openStartScreen();
+    }
+    public void openActivity4(){
+        Intent intent = new Intent(this, MainActivity4Q1.class);
+        startActivity(intent);
+    }
+
+    public void openStartScreen(){
+        Intent intent = new Intent(this, StartScreen.class);
+        startActivity(intent);
+    }
+}
---
+++ 10/ProjectDoulingo/app/src/main/java/com/example/doulingo/StartScreen.java
@@ -0,0 +1,52 @@
+package com.example.doulingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+        ImageView imageView1 = findViewById(R.id.imageView1);
+        ImageView imageView2 = findViewById(R.id.imageView2);
+        ImageView imageView3 = findViewById(R.id.imageView3);
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivityQ1();
+            }
+        });
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivityQ2();
+            }
+        });
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivityQ1();
+            }
+        });
+    }
+
+    public void openMainActivityQ1(){
+        Intent intent = new Intent(this, MainActivityQ1.class);
+        startActivity(intent);
+    }
+
+    public void openMainActivityQ2(){
+        Intent intent = new Intent(this, MainActivity2Q2.class);
+        startActivity(intent);
+    }
+}
+
+
+
