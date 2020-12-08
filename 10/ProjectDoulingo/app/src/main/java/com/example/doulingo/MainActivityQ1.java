858f6e3c5be6ff2ac743b4857875045db34847a6
DiemHieu <71001847+DiemHieu@users.noreply.github.com>
2020-12-09 00:22:28 +0700

    Update MainActivityQ1.java

--- 10/ProjectDoulingo/app/src/main/java/com/example/doulingo/MainActivityQ1.java
+++ 10/ProjectDoulingo/app/src/main/java/com/example/doulingo/MainActivityQ1.java
@@ -1,8 +1,10 @@
 package com.example.doulingo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,19 +27,23 @@ public class MainActivityQ1 extends AppCompatActivity {
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         textView3 = findViewById(R.id.textView3);
-
+        button4.setTextColor(Color.parseColor("#a9a9a9"));
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 button.setBackgroundResource(R.drawable.button_click_design);
+                button.setTextColor(Color.parseColor("#1e90ff"));
                 button1.setBackgroundResource(R.drawable.button_answer_design);
+                button1.setTextColor(Color.parseColor("#000000"));
                 button2.setBackgroundResource(R.drawable.button_answer_design);
+                button2.setTextColor(Color.parseColor("#000000"));
                 button4.setBackgroundResource(R.drawable.button_question_click_design);
+                button4.setTextColor(Color.parseColor("#ffffff"));
                 button4.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         String Button4Text = button4.getText().toString();
-                        textView3.setText("\t Chính xác!");
+                        textView3.setText("\t\t\n Chính xác!");
                         textView3.setBackgroundResource(R.drawable.text_design_true);
                         button4.setText("Tiếp tục");
                         if(Button4Text.equals("Tiếp tục")){
@@ -52,14 +58,18 @@ public class MainActivityQ1 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 button1.setBackgroundResource(R.drawable.button_click_design);
+                button1.setTextColor(Color.parseColor("#1e90ff"));
                 button.setBackgroundResource(R.drawable.button_answer_design);
+                button.setTextColor(Color.parseColor("#000000"));
                 button2.setBackgroundResource(R.drawable.button_answer_design);
+                button2.setTextColor(Color.parseColor("#000000"));
                 button4.setBackgroundResource(R.drawable.button_question_click_design);
+                button4.setTextColor(Color.parseColor("#ffffff"));
                 button4.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         String Button4Text = button4.getText().toString();
-                        textView3.setText("\t Sai rồi.Đáp án đúng là:\n  They drink juice ");
+                        textView3.setText("\t\t\n Sai rồi.Đáp án đúng là:\n\t  They drink juice ");
                         textView3.setBackgroundResource(R.drawable.text_design_false);
                         button4.setBackgroundResource(R.drawable.button_qs_click_false);
                         button4.setText("Tiếp tục");
@@ -74,15 +84,18 @@ public class MainActivityQ1 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 button2.setBackgroundResource(R.drawable.button_click_design);
+                button2.setTextColor(Color.parseColor("#1e90ff"));
                 button.setBackgroundResource(R.drawable.button_answer_design);
+                button.setTextColor(Color.parseColor("#000000"));
                 button1.setBackgroundResource(R.drawable.button_answer_design);
-
+                button1.setTextColor(Color.parseColor("#000000"));
                 button4.setBackgroundResource(R.drawable.button_question_click_design);
+                button4.setTextColor(Color.parseColor("#ffffff"));
                 button4.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         String Button4Text = button4.getText().toString();
-                        textView3.setText("\t Sai rồi.Đáp án đúng là:\n  They drink juice ");
+                        textView3.setText("\t\t\n Sai rồi.Đáp án đúng là:\n\t  They drink juice ");
                         textView3.setBackgroundResource(R.drawable.text_design_false);
                         button4.setBackgroundResource(R.drawable.button_qs_click_false);
                         button4.setText("Tiếp tục");
